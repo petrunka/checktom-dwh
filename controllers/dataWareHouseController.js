74c8e8282fafb3ee199612a512ba6a67e64506c7
petrunka <amo.dinamarca@abv.bg>
2015-02-13 11:13:06 +0200

    sending a response 200

--- controllers/dataWareHouseController.js
+++ controllers/dataWareHouseController.js
@@ -98,9 +98,12 @@ exports.vladimir = function (req, res) {
 };
 
 exports.usersLastMonth = function(req, res) {
-    DBpull.getUsersCreatedAt("2014-01-13","2015-02-13",function(data) {
-        res.send(data);
-        console.log(data);
+    DBpull.getUsersCreatedAt("2014-01-13","2015-02-13",function(err, data) {
+        if (err) {
+            res.send(200, err);
+        } else {
+            res.send(200, data);
+        }
     });
     console.log("data printed");
 };
